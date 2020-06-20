c3acb201e0e079f9286f6e0ea3333cc4447f5a0b
William Welling <wwelling@outlook.com>
2020-06-20 09:55:01 -0500

    switch back to root path

--- ldp/src/main/java/com/articulorum/ldp/controller/LdpController.java
+++ ldp/src/main/java/com/articulorum/ldp/controller/LdpController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException; 
+import javax.persistence.EntityNotFoundException;
 
 import com.articulorum.domain.Container;
 import com.articulorum.ldp.service.ContainerService;
@@ -27,14 +27,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 
 @RestController
-@RequestMapping("/api")
 public class LdpController {
 
     public final static String ROOT = SLASH;
